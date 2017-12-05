b7968ff0316fc52adc9dfc07720eb293dc1fa21c
hester1470 <546108933@qq.com>
2017-12-05 17:53:12 +0800

    11

--- js/ms_dec.js
+++ js/ms_dec.js
@@ -37,7 +37,6 @@ $(function(){
 	
 	$('#tree').treeview({data:getTree()}); 
 	$('#tree').on('nodeSelected', function(event, data) {
-		
 		$.ajax({
 				url:'json/dec1.json',     //陈向伟，传给你id  返回正文信息
 				type:'get',
@@ -62,76 +61,135 @@ $(function(){
 			});  
 	
 	});
-	 
-	$("#add").on("click",function(){
-		var arr=$('#tree').treeview('getChecked'); 
-	 	console.log(arr)
-		var singleNode = {  
-                    text: "小小" 
-                  };  
-        $("#tree").treeview("addNode", [singleNode,arr]);  
-	})
-})
+	
 
+
+})
+	function del(ele){
+		cancelBubble();
+		var pp= $('#tree').treeview('getSelected');
+		console.log(pp)
+		$('#tree').treeview('removeNode', [pp, { silent: true } ]);
+		
+	}
+//parentNode
+//targetNodes
+ function cancelBubble(e) { 
+    var evt = e ? e : window.event; 
+    if (evt.stopPropagation) {        //W3C 
+        evt.stopPropagation(); 
+    }else {       //IE      
+        evt.cancelBubble = true; 
+    }  
+ }
+	function add(ele){
+		$("#addModal").show();
+		$("#menuNameBtn").click(function(){
+			addMenu(ele);
+		})
+		 $("#menuName").keydown(function(k){
+			if(k.keyCode==13 ){
+				addMenu(ele);
+			}
+		});
+	}
+	
+	function addMenu(ele){
+		var value=$("#menuName").val();
+//		$.ajax({
+//			type:"(get)",
+//			url:"",
+//			data:{menuName:value},
+//			
+//		});
+		$('#addModal').modal('hide')
+		
+		var  parentId=ele.parent("li").attr("data-nodeid");
+		console.log(parentId)
+		var pp= $('#tree').treeview('getSelected');
+		console.log(pp)
+		var arr=$('#tree').treeview('getParents');
+		console.log(arr)
+		var singleNode = {  
+                    text: value ,
+                   	tags: ['+<span class="del" onclick="del($(this))">-</span>']
+                  };  
+        $("#tree").treeview("addNode", [singleNode,parentId,{ silent: true }]);  
+	}
+	
 	function getTree(){
 	   var tree = [
 		    {
 		    	text: "欢迎使用",
+		    	tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 		    	href:"100"
 		  	},
 		  	{
 		    	text: "注册及登录",
-		    	href:"200"
+		    	href:"200",
+		    	tags: ['+<span class="del" onclick="del($(this))">-</span>']
 		  	},
 		  	{
 		    	text: "首页",
-		    	href:"300"
+		    	href:"300",
+		    	tags: ['+<span class="del" onclick="del($(this))">-</span>']
 		  	},
 			{
 			    text: "数据分析",
 			    href:"400",
+			    tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 			    nodes: [
 				    {
 				        text: "我的目录",
+				        tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				        href:"410"
 				    },
 				    {
 				        text: "分析流程",
+				        tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				         href:"420",
 				        nodes: [
 				          {
 				            text: "分析流程类型",
+				            tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				            href:"421"
 				          },
 				          {
 				            text: "分析流程使用",
+				            tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				            href:"422"
 				          },
 				          {
 				            text: "深度挖掘使用",
+				            tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				            href:"423"
 				          }
 				        ]
 				    },
 				    {
 				        text: "我的项目",
+				        tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				        href:"430"
 				    },
 				    {
 				        text: "回收站",
+				        tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				        href:"440"
 				    }
 				    ,
 				    {
 				        text: "小工具",
+				        tags: ['+<span class="del" onclick="del($(this))">-</span>'], 
 				        href:"450",
 				        nodes: [
 					        {
 					            text: "小工具类型",
+					            tags: ['+<span class="del" onclick="del($(this))">-</span>'],  
 					            href:"451"
 					        },
 					        {
 					            text: "小工具使用",
+					            tags: ['+<span class="del" onclick="del($(this))">-</span>'], 
 					            href:"452"
 					        }
 				        ]
@@ -139,27 +197,31 @@ $(function(){
 				    ,
 				    {
 				        text: "资源监控",
+				        tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 				        href:"460"
 				    }
 			    ]
 			},
 			{
 			    text: "帮助",
+			    tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 			    href:"500"
 			},
 			{
 			    text: "管理系统",
+			    tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 			    href:"600"
 			},
 			{
 			    text: "退出",
+			    tags: ['+<span class="del" onclick="del($(this))">-</span>'],
 			    href:"700"
 			}
 		  
 		];
 		return tree;  
    }  
-   
+   	
    
 	function allParams(id){
 		var app = $("#"+id).serializeArray();
